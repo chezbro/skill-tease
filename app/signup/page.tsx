12914e68875873d339cd66655b32180ce6fe805f
chezbro <ericchesbrough@gmail.com>
2024-09-23 10:01:49 -0700

    vercel errors

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Register from '@/components/Register'
 import HeroCarousel from '@/components/HeroCarousel'
+import SignUpModal from '@/components/SignUpModal'
 
 export default function SignUpPage() {
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false)
+  }
+
   return (
     <div className="flex flex-col md:flex-row h-screen">
       <div className="w-full md:w-1/2 p-8">
@@ -11,6 +18,9 @@ export default function SignUpPage() {
       <div className="w-full md:w-1/2">
         <HeroCarousel />
       </div>
+      {isModalOpen && (
+        <SignUpModal onClose={handleCloseModal} />
+      )}
     </div>
   )
 }
